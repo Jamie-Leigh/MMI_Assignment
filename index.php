5543217590d146f09f357c254004b835cdcd985c
Jamie Leigh <s210901@uos.ac.uk>
2021-12-02 21:50:35 +0000

    404 page

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if($_SESSION['user_data']) {
 }
 ini_set('display_errors', 'On');
 $page = $_GET['p'];
+$pages = ['account', 'basket', 'car', 'checkout', 'editprofileimage', 'faq', 'home', 'login', 'logout', 'results', 'search'];
 $isUser = ($_SESSION['user_data']['user_type'] == 'USER');
 $isAdmin = ($_SESSION['user_data']['user_type'] == 'ADMIN');
 $isSuper = ($_SESSION['user_data']['user_type'] == 'SUPER');
@@ -20,9 +21,6 @@ $visitor_notallowed = ['caradmin', 'useradmin', 'basket', 'account'];
 $user_notallowed = ['caradmin', 'useradmin'];
 $admin_notallowed = ['caradmin'];
 
-if(!$page){
-    $page = "home";
-}
 
 
 if($isVisitor) {
@@ -30,19 +28,25 @@ if($isVisitor) {
         echo "not allowed";
         exit();
     }
-}else if($isUser) {
+} else if($isUser) {
     if(in_array($page, $user_notallowed)) {
         echo "not allowed";
         exit();
     }
-}else if($isAdmin) {
+} else if($isAdmin) {
     if(in_array($page, $admin_notallowed)) {
         echo "not allowed";
         exit();
     }
 }
+if(!$page){
+    $page = "home";
+} else if (!(in_array($page, $pages))) {
+    $page = '404';
+}
 
-//n assume allowed
+
+// assume allowed
 require_once(__DIR__.'/includes/header.php');
 require_once(__DIR__.'/pages/'.$page.'.php');
 require_once(__DIR__.'/includes/footer.php');
